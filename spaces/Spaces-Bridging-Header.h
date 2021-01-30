db3860c89199e6091f8e353ea1128a0c83543253
Kristian Boda <kboda+git@protonmail.com>
2021-01-30 16:39:23 +0000

    feat: spaces

---
+++ spaces/Spaces-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Spaces-Bridging-Header.h
+//  spaces
+//
+//  Created by Kristian Boda on 30/01/2021.
+//
+
+@import Foundation;
+
+int _CGSDefaultConnection();
+id CGSCopyActiveMenuBarDisplayIdentifier(int conn);
+id CGSCopyManagedDisplaySpaces(int conn);
